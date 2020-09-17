dca0da9353df2eec44906f1a28cc107e2823cc2c
Nick Lonz <nicklonz@gmail.com>
2020-09-17 09:25:24 -0400

    RR 18

--- app/javascript/components/Hello.jsx
+++ app/javascript/components/Hello.jsx
@@ -11,16 +11,11 @@ const Hello = props => (
 )
 
 Hello.defaultProps = {
-  name: 'David'
+  name: 'Nick'
 }
 
 Hello.propTypes = {
   name: PropTypes.string
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="Nick - from React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+export default Hello
